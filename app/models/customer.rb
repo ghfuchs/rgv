abec6b25528f604c2eacb7f3bbaeebb29a86f0a6
ghfuchs <george-fuchs@sbcglobal.net>
2011-06-07 01:02:35 -0500

    06/07/2011  1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
   has_many :Orders
+  has_many :reviews
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ActiveRecord::Base
   has_many :menus
   has_many :orders
+  has_many :reviews
   validates :name, :address, :city, :state, :presence => true
   validates :name, :uniqueness => true
 
