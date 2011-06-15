4d9a73640a9907687634efde15520e9aa7930e88
ghfuchs <george-fuchs@sbcglobal.net>
2011-06-15 13:20:59 -0500

    06/15/2011  2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rgv::Application.routes.draw do
 
-  root :to => "home#index"
-
   devise_for :admins
 
   devise_for :users
@@ -76,4 +74,7 @@ Rgv::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  root :to => "home#index"
+
 end
