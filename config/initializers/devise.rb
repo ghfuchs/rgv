ab6e64b7ea58c6f00a27ae12b0ef2b11323525fe
ghfuchs <george-fuchs@sbcglobal.net>
2011-06-14 21:48:38 -0500

    New changelist

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -126,6 +126,7 @@ Devise.setup do |config|
   # Defines which key will be used when recovering the password for an account
   # config.reset_password_keys = [ :email ]
 
+
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
